cdea8ea9462134ac7d51fcd427687df7d2d9ba6e
punk <punk.dvpn@proton.me>
2025-07-09 11:15:58 +0300

    Added libtailscale.aar to the publication

--- sources/library/build.gradle.kts
+++ sources/library/build.gradle.kts
@@ -5,13 +5,44 @@ plugins {
 
 android.namespace = "app.dvpn.libtailscale"
 
+val localAar = configurations.create("localAar")
+
 dependencies {
-    api(fileTree("${rootDir}/libs") {
-        setIncludes(listOf("*aar", "*.jar"))
-    })
+    localAar(files("${rootDir}/libs/libtailscale.aar"))
+    
+    // Regular dependencies
     implementation(libs.androidxCoreKtx)
     implementation(libs.kotlinCoroutinesCore)
     implementation(libs.kotlinSerialization)
     implementation(libs.okhttp)
     implementation(libs.timber)
+}
+
+val extractAar = tasks.register<Copy>("extractAar") {
+    from(zipTree(configurations["localAar"].singleFile))
+    into("${buildDir}/extracted-aar")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    dependsOn(extractAar)
+}
+
+android {
+    sourceSets {
+        getByName("main") {
+            java.srcDir("${buildDir}/extracted-aar/classes")
+            res.srcDir("${buildDir}/extracted-aar/res")
+            assets.srcDir("${buildDir}/extracted-aar/assets")
+            jniLibs.srcDir("${buildDir}/extracted-aar/jni")
+        }
+    }
+}
+
+afterEvaluate {
+    tasks.named("preBuild").configure {
+        dependsOn(extractAar)
+    }
+    dependencies {
+        compileOnly(files("${buildDir}/extracted-aar/classes.jar"))
+    }
 }
